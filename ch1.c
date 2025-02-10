3d70de1947e2c3b907acd8e629da8b31a45bdb16
Chris Sinclair <chrissinclair89@gmail.com>
2025-02-10 01:28:48 -0600

    Added exercise 1-10

--- ch1.c
+++ ch1.c
@@ -23,7 +23,7 @@ void escapeChar() {
 void tempConversion1() {
 	float fahr, cels;
 	int lower = 0, upper = 100, step = 20;
-	printf("Temperature Conversion: Fahrenheit to Celsius\n");
+	printf("Temperature Conversion: Fahrenheit to Celsius:\n");
 	while (fahr <= upper) {
 		cels = (5.0 / 9.0) * (fahr - 32.0);
 		printf("Fahrenheit: %.0f\t\tCelsius: %.2f\n", fahr, cels);
@@ -38,7 +38,7 @@ void tempConversion1() {
 void tempConversion2() {
 	float fahr, cels;
 	int lower = 0, upper = 5, step = 1;
-	printf("Temperature Conversion: Celsius to Fahrenheit\n");
+	printf("Temperature Conversion: Celsius to Fahrenheit:\n");
 	cels = 0.0;
 	while (cels <= upper) {
 		fahr = (cels * (9.0/5.0)) + 32;
@@ -52,25 +52,89 @@ void tempConversion2() {
  * Exercise 1-5
  */
 
+void tempConversion3() {
+	float fahr, cels;
+	int lower = 0, upper = 5, step = 1;
+	printf("Temperature Conversion: Celsius to Fahrenheit (reverse order):\n");
+	cels = upper;
+	while (cels >= lower) {
+		fahr = (cels * (9.0/5.0)) + 32;
+		printf("Celsius: %.0f\t\tFahrenheit: %.1f\n", cels, fahr);
+		cels -= step;
+	}
+}
+
+
 /*
  * Exercise 1-6
  */
+
+void fileCopying1() {
+	printf("Either enter a character below or press Ctrl+D for no characters:\n");
+	int c = getchar() != EOF;
+	printf("getchar = %d\n", c);
+}
 
 /*
  * Exercise 1-7
  */
 
+void fileCopying2() {
+	int c = EOF;
+	printf("getchar = %d\n", c);
+}
+
 /*
  * Exercise 1-8
  */
+
+void charCounting1() {
+	int blankCount = 0, tabCount = 0, lineCount = 1;
+	int c;
+	while ((c = getchar()) != EOF) {
+		if (c == '\n') lineCount++;
+		if (c == '\t') tabCount++;
+		if (c == ' ') blankCount++;
+	}
+	printf("\n");
+	printf("Number of blanks = %d\n", blankCount);
+	printf("Number of tabs = %d\n", tabCount);
+	printf("Number of lines = %d\n", lineCount);	
+}
 
 /*
  * Exercise 1-9
  */
 
+void charCounting2() {
+	int c, prev = 0;
+	while ((c = getchar()) != EOF) {
+		if (c != ' ' || prev != ' ') putchar(c);
+		prev = c;
+	}
+	printf("\n");
+}
+
 /*
  * Exercise 1-10
  */
+
+void charCounting3() {
+	int c;
+	while ((c = getchar()) != EOF) {
+		if (c == '\t') {
+			putchar('\\');
+			putchar('t');
+		} else if (c == '\b') {
+			putchar('\\');
+			putchar('b');
+		} else if (c == '\\') {
+			putchar('\\');
+			putchar('\\');
+		} else putchar(c);
+	}
+	printf("\n");
+}
 
 /*
  * Exercise 1-11
@@ -134,6 +198,11 @@ int main() {
 	escapeChar();
 	tempConversion1();
 	tempConversion2();
-
+	tempConversion3();
+//	fileCopying1();
+//	fileCopying2();
+//	charCounting1();
+//	charCounting2();
+	charCounting3();
 	return 0;
 }
